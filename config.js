88d2219de217497a54ededbc0805a664acb16fce
cxk <cxk0831@163.com>
2020-09-11 19:01:13 +0800

    feat: 增加用户配置文件
    增加用户配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 主项目本地绝对路径
+    projectAddress: '/Users/cxk/project/react-native/chatDemo',
+    // 库名字
+    libraryName: 'veronica-F2',
+    // 发布npm上的库前缀
+    libraryNamePrefix: '@sishu',
+    // 发布npm上的库版本
+    libraryVersion: '0.0.4',
+    // 发布npm上的库的描述
+    libraryDescription: 'F2报表库',
+}
